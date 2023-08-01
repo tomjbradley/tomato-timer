ff94e1b7eef3254232b7197061d91c5428b25adb
Tom Bradley <tomjosephbradley@gmail.com>
2023-08-01 12:39:47 +0100

    adds modals

--- main.js
+++ main.js
@@ -1,36 +1,54 @@
-// Constants/Magic
+// Constants
 const POMODORO = 25 * 60;
 const SHORT_BREAK = 5 * 60;
 const LONG_BREAK = 10 * 60;
 
-// Dictionaries
 const timerDictionary = {
   pomodoro: POMODORO,
   shortBreak: SHORT_BREAK,
   longBreak: LONG_BREAK,
 };
 
-// DOM Elements
+const defaultSettings = {
+  pomodoroGoal: 5,
+};
+
+// User Interface
 const timer = document.getElementById("timer");
+const logTableBody = document.querySelector("#log-table tbody");
+
 const timerButtons = Array.from(
   document.getElementById("timer-buttons").children
 );
 const startButton = document.getElementById("start-button");
 const stopButton = document.getElementById("stop-button");
 const resetButton = document.getElementById("reset-button");
+const toggleModalButtons = document.querySelectorAll("[data-toggle='modal']");
+const pomodoroGoal = document.getElementById("pomodoro-goal");
+const clearLogButton = document.getElementById("clear-log-button");
+const clearTodaysSessionsButton = document.getElementById(
+  "clear-todays-sessions-button"
+);
+const soundTestButton = document.getElementById("sound-test-button");
+const soundSelect = document.getElementById("sound-select");
+const volumeSelect = document.getElementById("volume-select");
 const enableNotificationsButton = document.getElementById(
   "enable-notifications-button"
 );
 
-// User Interactions
-timerButtons.forEach((timerButton) => {
-  timerButton.addEventListener("click", () =>
-    changeTimer(timerButton.dataset.value)
+document.addEventListener("DOMContentLoaded", () => {
+  const sessions = JSON.parse(localStorage.getItem("sessions")) ?? [];
+  sessions.forEach((session) => addLogRow(session));
+
+  const pomodoroGoal = 12 ?? defaultSettings.pomodoroGoal;
+  loop(pomodoroGoal, addGoalIndicator);
+  const todaysDate = new Date().getDate();
+  const todaysPomodoros = sessions.filter(
+    (session) =>
+      !(session.type === pomodoro && session.startTime.getDate() === todaysDate)
   );
+  loop(todaysPomodoros.length, activateNextGoalIndicator);
 });
-addKeyboardShortcut("KeyP", { altKey: true }, () => changeTimer("pomodoro"));
-addKeyboardShortcut("KeyS", { altKey: true }, () => changeTimer("shortBreak"));
-addKeyboardShortcut("KeyL", { altKey: true }, () => changeTimer("longBreak"));
 
 startButton.addEventListener("click", () => startTimer());
 stopButton.addEventListener("click", () => stopTimer());
@@ -45,22 +63,90 @@ addKeyboardShortcut("Space", {}, () => {
 resetButton.addEventListener("click", () => resetTimer());
 addKeyboardShortcut("KeyR", { altKey: true }, () => resetTimer());
 
-enableNotificationsButton.addEventListener("click", () =>
-  Notification.requestPermission()
+timerButtons.forEach((timerButton) => {
+  timerButton.addEventListener("click", () =>
+    changeTimer(timerButton.dataset.value)
+  );
+});
+addKeyboardShortcut("KeyP", { altKey: true }, () => changeTimer("pomodoro"));
+addKeyboardShortcut("KeyS", { altKey: true }, () => changeTimer("shortBreak"));
+addKeyboardShortcut("KeyL", { altKey: true }, () => changeTimer("longBreak"));
+
+toggleModalButtons.forEach((toggleButton) => {
+  const modal = document.getElementById(toggleButton.dataset.target);
+  const closeModalButton = modal.querySelector("[data-dismiss='modal'");
+
+  toggleButton.addEventListener("click", () => modal.showModal());
+  closeModalButton.addEventListener("click", () => modal.close());
+  modal.addEventListener("click", (event) => {
+    const dialogDimensions = modal.getBoundingClientRect();
+    if (
+      event.clientX < dialogDimensions.left ||
+      event.clientX > dialogDimensions.right ||
+      event.clientY < dialogDimensions.top ||
+      event.clientY > dialogDimensions.bottom
+    ) {
+      modal.close();
+    }
+  });
+});
+
+clearLogButton.addEventListener("click", () => clearLog());
+clearTodaysSessionsButton.addEventListener("click", () =>
+  clearTodaysSessions()
 );
 
-// App State
+soundTestButton.addEventListener("click", () => triggerSoundTest());
+
+enableNotificationsButton.addEventListener("click", () =>
+  requestNotificationPermission()
+);
+
+// Rendering Helpers
+function addLogRow(session) {
+  const logTableRow = document.createElement("tr");
+  logTableRow.setAttribute("key", session.id);
+  logTableRow.innerHTML = `
+    <td>${session.type}</td>
+    <td>${formatDate(session.startTime)}</td>
+    <td>${formatDate(session.endTime)}</td>
+    <td><textarea value='${session.description}'></textarea></td>
+  `;
+
+  logTableBody.append(logTableRow);
+}
+function addGoalIndicator() {
+  const goalIndicator = document.createElement("span");
+  pomodoroGoal.append(goalIndicator);
+}
+function activateNextGoalIndicator() {
+  const pomodoroGoalIndicators = Array.from(pomodoroGoal.children);
+
+  pomodoroGoalIndicators.every((goalIndicator) => {
+    if (!goalIndicator.classList.contains("active")) {
+      goalIndicator.classList.add("active");
+      return false;
+    }
+
+    return true;
+  });
+}
+function removeLogRow(id) {
+  const logTableRow = document.querySelector(`[key='${id}']`);
+  logTableRow.remove();
+}
+
+// Logic
 let isRunning = false;
+let timerSeconds = 25 * 60;
+let currentTimer = "pomodoro";
+let sessions = JSON.parse(localStorage.getItem("sessions")) ?? [];
 
-const state = { timerSeconds: 25 * 60, currentTimer: "pomodoro" };
-
-// UI Updaters
 function updateTimerSeconds(seconds) {
-  state.timerSeconds = seconds;
+  timerSeconds = seconds;
   timer.textContent = formatTime(seconds);
 }
 
-// Data Formatters
 function formatTime(seconds) {
   const minutes = Math.floor(seconds / 60);
   const secondsInMinute = seconds % 60;
@@ -70,20 +156,62 @@ function formatTime(seconds) {
   }`;
 }
 
+const dateFormatter = new Intl.DateTimeFormat(undefined, {
+  dateStyle: "full",
+  timeStyle: "short",
+});
+function formatDate(date) {
+  return dateFormatter.format(date);
+}
+
 // Core Functionality
+let currentSession = {};
+function clearLog() {
+  localStorage.removeItem("sessions");
+  logTableBody.innerHTML = "";
+}
+function clearTodaysSessions() {
+  const todaysDate = new Date().getDate();
+  const todaysSessions = sessions.filter(
+    (session) => new Date(session.startTime).getDate() === todaysDate
+  );
+
+  todaysSessions.forEach((session) => removeLogRow(session.id));
+
+  sessions = sessions.filter((session) => todaysSessions.indexOf(session) < 0);
+  localStorage.setItem("sessions", JSON.stringify(sessions));
+}
+function addSession(session) {
+  session.endTime = Date.now();
+  session.description = "";
+
+  addLogRow(session);
+  activateNextGoalIndicator();
+
+  sessions.push(session);
+  localStorage.setItem("sessions", JSON.stringify(sessions));
+
+  currentSession = {};
+}
+
 let interval;
 function startTimer() {
   if (!isRunning) {
+    currentSession.id = sessions.length + 1;
+    currentSession.type = currentTimer;
+    currentSession.startTime = Date.now();
+
     isRunning = true;
     interval = setInterval(() => {
-      updateTimerSeconds(state.timerSeconds - 1);
+      updateTimerSeconds(timerSeconds - 1);
 
-      if (state.timerSeconds <= 0) {
+      if (timerSeconds <= 0) {
         clearInterval(interval);
         triggerNotification();
         playAlarmSound("alarmwatch.mp3", 0.5);
+        addSession(currentSession);
       }
-    }, 1000);
+    }, 10);
   }
 }
 function stopTimer() {
@@ -92,19 +220,26 @@ function stopTimer() {
 }
 function resetTimer() {
   stopTimer();
-  updateTimerSeconds(timerDictionary[state.currentTimer]);
+  updateTimerSeconds(timerDictionary[currentTimer]);
 }
 function changeTimer(nextTimer) {
-  state.currentTimer = nextTimer;
+  currentTimer = nextTimer;
   resetTimer();
   startTimer();
 }
+function triggerSoundTest() {
+  playAlarmSound(soundSelect.value, volumeSelect.value);
+}
 
 // Web APIs
+function requestNotificationPermission() {
+  Notification.requestPermission();
+}
+
 function triggerNotification() {
   if (Notification.permission === "granted") {
     const notification = new Notification("TomatoTimer", {
-      icon: "/logo192.png",
+      icon: "/logo.png",
       body: "Your time is up!!",
     });
 
@@ -113,7 +248,7 @@ function triggerNotification() {
 }
 
 function playAlarmSound(filename, volume) {
-  const audio = Audio("/assets/audio/" + filename);
+  const audio = new Audio("/assets/audio/" + filename);
   audio.volume = volume;
   audio.play();
 }
@@ -130,4 +265,10 @@ function addKeyboardShortcut(keyCode, modifiers, callback) {
       callback();
     }
   });
+}
+
+function loop(times, callback) {
+  for (let i = 0; i < times; i++) {
+    callback();
+  }
 }
